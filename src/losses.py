a963f18db21d10cfb9a8326104834b389d605566
Tuan Do <minhtuan90797@gmail.com>
2025-12-28 17:53:37 -0600

    Add Losses

--- src/losses.py
+++ src/losses.py
@@ -10,3 +10,23 @@ class Loss:
     def compute_gradient(self, y_pred, y_train):
         raise NotImplementedError
     
+class MSELoss(Loss):
+    def __init__(self):
+        super().__init__()
+
+    def compute_value(self, y_pred, y_train):
+        return np.sum((y_pred - y_train) * (y_pred - y_train))/y_train.shape[0]
+
+    def compute_gradient(self, y_pred, y_train):
+        return np.sum(y_pred - y_train, axis = 0)/y_train.shape[0]
+
+class CrossEntropyLoss(Loss):
+    def __init__(self):
+        super().__init__()
+
+    def compute_value(self, y_pred, y_train):
+        return -(np.sum(y_train * np.log(y_pred), axis = 0))/y_train.shape[0]
+
+    def compute_gradient(self, y_pred, y_train):
+        return -(np.sum(y_train/y_pred, axis = 0))/y_train.shape[0]
+    
